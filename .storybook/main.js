c126b37aaf74abd49c1aa6082e34d08966ce9cea
maeek <maciej@suchanecki.me>
2022-12-19 13:01:57 +0100

    chore: updated storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,21 +1,19 @@
 const path = require('path');
-
 module.exports = {
-  stories: [
-    '../src/**/*.stories.mdx',
-    '../src/**/*.stories.@(js|jsx|ts|tsx)'
-  ],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-  ],
-  webpackFinal: async (config, { configType }) => {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  core: {
+    disableTelemetry: true,
+    builder: 'webpack5'
+  },
+  webpackFinal: async (config, {
+    configType
+  }) => {
     config.module.rules.push({
       test: /\.scss$/,
       use: ['style-loader', 'css-loader', 'sass-loader'],
-      include: path.resolve(__dirname, '../'),
+      include: path.resolve(__dirname, '../')
     });
-
     return config;
   }
-}
+};
